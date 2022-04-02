fb1113446f6d8c8b8022165f09e4fc290e5c1223
sergeblr2@gmail.com <Soska65cs>
2022-04-02 18:51:45 +0300

    WildFly run: worked rest-app (added SpringBootServletInitializer and pom changes) and web-app

--- web-app/src/main/java/by/bhl/web/WebApp.java
+++ web-app/src/main/java/by/bhl/web/WebApp.java
@@ -3,14 +3,21 @@ package by.bhl.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} )
-public class WebApp
+public class WebApp extends SpringBootServletInitializer
 {
 
     public static void main( String[] args )
     {
         SpringApplication.run(WebApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApp.class);
     }
 
 }
