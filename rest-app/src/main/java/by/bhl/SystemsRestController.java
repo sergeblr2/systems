c068ee0d49639b90c390e53ba364801cb302e00e
sergeblr2@gmail.com <Soska65cs>
2022-05-30 16:49:21 +0300

    rest/web uri's refactor to work both wildfly and beanstalk

--- rest-app/src/main/java/by/bhl/SystemsRestController.java
+++ rest-app/src/main/java/by/bhl/SystemsRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/rest/pcs")
+@RequestMapping("/pcs")
 public class SystemsRestController {
 
     Logger logger = LoggerFactory.getLogger(SystemsRestController.class);
