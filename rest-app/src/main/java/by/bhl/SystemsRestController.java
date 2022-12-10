5a6bd61ada4c11e26ee2a11f6cbce27980c09d8c
sergeblr2@gmail.com <Soska65cs>
2022-12-10 13:02:09 +0300

    updated post to consume json

--- rest-app/src/main/java/by/bhl/SystemsRestController.java
+++ rest-app/src/main/java/by/bhl/SystemsRestController.java
@@ -5,6 +5,7 @@ import by.bhl.repository.PersonalComputerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,7 +41,7 @@ public class SystemsRestController {
         return repository.findByNameContains(value);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addPersonalComputer(@RequestBody PersonalComputer personalComputer) {
         logger.info("Saving pc: " + personalComputer.toString());
         repository.save(personalComputer);
